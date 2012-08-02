6718fce816c88d1e877eddedba53610ca9afdd75
Kevin Peterson <kevin.peterson@mayo.edu>
2012-08-01 22:33:06 -0500

    Added some more tests.

--- src/main/scala/edu/mayo/cts2/framework/plugin/service/nlm/profile/valuesetdefinition/PhinVadsValueSetDefinitionResolutionService.scala
+++ src/main/scala/edu/mayo/cts2/framework/plugin/service/nlm/profile/valuesetdefinition/PhinVadsValueSetDefinitionResolutionService.scala
@@ -1,24 +1,27 @@
 package edu.mayo.cts2.framework.plugin.service.nlm.profile.valuesetdefinition
 
-import java.lang.Override
-import scala.collection.JavaConversions._
-import org.springframework.stereotype.Component
-import edu.mayo.cts2.framework.model.codesystemversion.CodeSystemVersionCatalogEntry
-import edu.mayo.cts2.framework.model.service.core.NameOrURI
-import edu.mayo.cts2.framework.plugin.service.nlm.profile.AbstractService
-import edu.mayo.cts2.framework.service.profile.codesystemversion.CodeSystemVersionReadService
-import edu.mayo.cts2.framework.model.extension.LocalIdValueSetDefinition
-import edu.mayo.cts2.framework.service.profile.valuesetdefinition.name.ValueSetDefinitionReadId
-import edu.mayo.cts2.framework.plugin.service.nlm.phinvads.dao.PhinVadsDao
-import javax.annotation.Resource
-import gov.cdc.vocab.service.dto.input.ValueSetVersionSearchCriteriaDto
 import java.util.Set
-import edu.mayo.cts2.framework.model.valuesetdefinition.{ ResolvedValueSet, ValueSetDefinition }
-import edu.mayo.cts2.framework.model.command.{ Page, ResolvedReadContext }
-import edu.mayo.cts2.framework.service.profile.valuesetdefinition.{ ResolvedValueSetResult, ResolvedValueSetResolutionEntityQuery, ValueSetDefinitionReadService, ValueSetDefinitionResolutionService }
-import edu.mayo.cts2.framework.model.entity.EntityDirectoryEntry
+
+import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+
+import org.springframework.stereotype.Component
+
+import edu.mayo.cts2.framework.model.command.Page
+import edu.mayo.cts2.framework.model.command.ResolvedReadContext
 import edu.mayo.cts2.framework.model.core._
+import edu.mayo.cts2.framework.model.entity.EntityDirectoryEntry
+import edu.mayo.cts2.framework.model.service.core.NameOrURI
+import edu.mayo.cts2.framework.model.valuesetdefinition.ResolvedValueSet
+import edu.mayo.cts2.framework.model.valuesetdefinition.ResolvedValueSetHeader
+import edu.mayo.cts2.framework.plugin.service.nlm.phinvads.dao.PhinVadsDao
+import edu.mayo.cts2.framework.plugin.service.nlm.profile.AbstractService
+import edu.mayo.cts2.framework.service.profile.valuesetdefinition.ResolvedValueSetResolutionEntityQuery
+import edu.mayo.cts2.framework.service.profile.valuesetdefinition.ResolvedValueSetResult
+import edu.mayo.cts2.framework.service.profile.valuesetdefinition.ValueSetDefinitionResolutionService
+import edu.mayo.cts2.framework.service.profile.valuesetdefinition.name.ValueSetDefinitionReadId
 import gov.cdc.vocab.service.bean.ValueSetConcept
+import javax.annotation.Resource
 
 @Component
 class PhinVadsValueSetDefinitionResolutionService extends AbstractService with ValueSetDefinitionResolutionService {
@@ -52,18 +55,33 @@ class PhinVadsValueSetDefinitionResolutionService extends AbstractService with V
 
     val versionId = valueSetVersion.getValueSetVersion().getId()
 
-    val synopsis: Seq[EntitySynopsis] = phinVadsDao.vocabService.
+    val valueSetConcepts = phinVadsDao.vocabService.
       getValueSetConceptsByValueSetVersionId(versionId, 1, 50).
-      getValueSetConcepts().map(transformValueSetConcept)
+      getValueSetConcepts().asScala
+      
+    val synopsis: Seq[EntitySynopsis] = valueSetConcepts.map(transformValueSetConcept)
 
-    new ResolvedValueSetResult(null, synopsis, true)
+    new ResolvedValueSetResult(buildHeader(), synopsis, true)
   }
 
+  private def buildHeader(): ResolvedValueSetHeader = {
+    val header = new ResolvedValueSetHeader()
+    
+    val valueDefSetRef = new ValueSetDefinitionReference()
+    valueDefSetRef.setValueSet(new ValueSetReference("unknown"))
+    valueDefSetRef.setValueSetDefinition(new NameAndMeaningReference("unknown"))
+    header.setResolutionOf(valueDefSetRef)
+    
+    header
+  }
+  
   private def transformValueSetConcept = (entry: ValueSetConcept) => {
     val synopsis: EntitySynopsis = new EntitySynopsis()
     synopsis.setDesignation(entry.getCdcPreferredDesignation())
     synopsis.setName(entry.getConceptCode())
-    synopsis.setNamespace(entry.getCodeSystemConceptName())
+    synopsis.setNamespace("ns")
+    synopsis.setUri("uri:urn:" + entry.getId);
+    
     synopsis
   }: EntitySynopsis
 
--- src/test/groovy/edu/mayo/cts2/framework/plugin/service/nlm/profile/valuesetdefinition/PhinVadsValueSetDefinitionResolutionServiceTestIT.groovy
+++ src/test/groovy/edu/mayo/cts2/framework/plugin/service/nlm/profile/valuesetdefinition/PhinVadsValueSetDefinitionResolutionServiceTestIT.groovy
@@ -1,6 +1,7 @@
 package edu.mayo.cts2.framework.plugin.service.nlm.profile.valuesetdefinition
 
 import javax.annotation.Resource
+import javax.xml.transform.stream.StreamResult
 
 import org.junit.Test
 
@@ -8,7 +9,6 @@ import edu.mayo.cts2.framework.core.xml.DelegatingMarshaller
 import edu.mayo.cts2.framework.model.command.Page
 import edu.mayo.cts2.framework.model.util.ModelUtils
 import edu.mayo.cts2.framework.plugin.service.nlm.test.AbstractTestBase
-import edu.mayo.cts2.framework.service.profile.valueset.ValueSetQuery
 import edu.mayo.cts2.framework.service.profile.valuesetdefinition.name.ValueSetDefinitionReadId
 
 class PhinVadsValueSetDefinitionResolutionServiceTestIT extends AbstractTestBase {
@@ -29,6 +29,51 @@ class PhinVadsValueSetDefinitionResolutionServiceTestIT extends AbstractTestBase
 		def id = new ValueSetDefinitionReadId("1", ModelUtils.nameOrUriFromName("PHVS_AbnormalFlag_HL7_2x"))
 		
 		assertNotNull service.resolveDefinition(id, null, null, null , null, null, new Page());
+	}
+	
+	@Test
+	void TestGetSummariesSize() {
+		def id = new ValueSetDefinitionReadId("1", ModelUtils.nameOrUriFromName("PHVS_AbnormalFlag_HL7_2x"))
+		
+		assertTrue service.resolveDefinition(id, null, null, null , null, null, new Page()).entries.size() > 1
+	}
+	
+	@Test
+	void TestGetSummariesSizeDifferent() {
+		def id = new ValueSetDefinitionReadId("1", ModelUtils.nameOrUriFromName("PHVS_AdministrativeGender_HL7_V3"))
+		
+		assertTrue service.resolveDefinition(id, null, null, null , null, null, new Page()).entries.size() > 1
+	}
+	
+	@Test
+	void TestGetSummariesEntriesValidXml() {
+		def id = new ValueSetDefinitionReadId("1", ModelUtils.nameOrUriFromName("PHVS_AbnormalFlag_HL7_2x"))
+		
+		def entries = service.resolveDefinition(id, null, null, null , null, null, new Page()).entries
+		
+		assertTrue entries.size() > 1
+		
+		entries.each {
+			marshaller.marshal(it, new StreamResult(new StringWriter()))
+		}
+	}
+	
+	@Test
+	void TestGetSummariesHeadingNotNull() {
+		def id = new ValueSetDefinitionReadId("1", ModelUtils.nameOrUriFromName("PHVS_AbnormalFlag_HL7_2x"))
+		
+		def heading = service.resolveDefinition(id, null, null, null , null, null, new Page()).resolvedValueSetHeader
+		
+		assertNotNull heading
+	}
+	
+	@Test
+	void TestGetSummariesHeadingValidXml() {
+		def id = new ValueSetDefinitionReadId("1", ModelUtils.nameOrUriFromName("PHVS_AbnormalFlag_HL7_2x"))
+		
+		def heading = service.resolveDefinition(id, null, null, null , null, null, new Page()).resolvedValueSetHeader
+		
+		marshaller.marshal(heading, new StreamResult(new StringWriter()))
 	}
 
 }
